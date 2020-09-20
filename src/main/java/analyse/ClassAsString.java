bcebba02bbc37d5bdccb87f65f89254efbbac880
greenpanther93 <pascal.zaragoza@yahoo.com>
2020-09-20 18:09:19 +0200

    fixed Personne class

--- src/main/java/analyse/ClassAsString.java
+++ src/main/java/analyse/ClassAsString.java
@@ -4,14 +4,14 @@ public class ClassAsString {
 	
 	public static String classAsString = 
 	"public class Personne{ "
-			+ "\\npublic int age;"
-			+ "\\npublic String nom;"
-			+ "\\npublic String prenom;"
-			+ "\\npublic Personne(int age, String nom, String prenom) {"
-				+ "\\nthis.age = age;"
-				+ "\\nthis.nom = nom;"
-				+ "\\nthis.prenom = prenom;"
-			+ "\\n}"
-	+ "\\n}";
+			+ "\npublic int age;"
+			+ "\npublic String nom;"
+			+ "\npublic String prenom;"
+			+ "\npublic Personne(int age, String nom, String prenom) {"
+				+ "\nthis.age = age;"
+				+ "\nthis.nom = nom;"
+				+ "\nthis.prenom = prenom;"
+			+ "\n}"
+	+ "\n}";
 
 }
